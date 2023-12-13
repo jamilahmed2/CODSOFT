a52eb270445bf29d372312c93fab032a23bf9575
jamilahmed2 <ja0935991@gmail.com>
2023-12-14 01:52:59 +0500

    trave-booking-app

---
+++ travel-booking-app/frontend/src/pages/UserProfile.js
@@ -0,0 +1,22 @@
+import './userDashboard.css'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext.js'
+
+const UserProfile = () => {
+
+    const { user } = useContext(AuthContext);
+    const avatar = <i class="fa-regular fa-user"></i>
+    return (
+        <>
+            <section className="user-dashbaord">
+                <div className="user-image">
+                    {user.photo || avatar}
+                </div>
+                <h2>{user.name}</h2>
+                <h2>{user.email}</h2>
+            </section>
+        </>
+    )
+}
+
+export default UserProfile
